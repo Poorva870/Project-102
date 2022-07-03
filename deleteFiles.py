2121c2eaf5f2dd89eaadd953c665c35d25027e85
Poorva870 <80689366+Poorva870@users.noreply.github.com>
2022-07-03 15:29:46 +0530

    Add files via upload

---
+++ deleteFiles.py
@@ -0,0 +1,12 @@
+import os
+ # Walking through a folder tree
+for folders,subfolders,filenames in os.walk('/home/nitin'):
+    for folder in folders: 
+        print 'The current folder is :' + folder 
+    for subfolder in subfolders: 
+        print 'The subfolder of' + folder + ':' + subfolder
+    for filename in filenames: 
+        print 'The file inside' + subfolder + ':' + filename a = os.path.getsize(filename) 
+
+    print ('The file size is ' + a ) 
+    print ('The file path is ' + os.path.abspath('./filename'))
